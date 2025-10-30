da20b01e23eb67e17ca9d41a69d82691e6ae1866
vkrotova <violettakrotova23@gmail.com>
2025-10-30 15:52:43 -0700

    Assiged the attributes to creted checkbox

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function addElement(name, listname) {
      let newListItem = document.createElement('li');
       let checkbox = document.createElement("input");
 
-
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = "id";
 
 
 }
