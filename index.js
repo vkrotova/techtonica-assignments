a3ba422f30de77c958ac8c7eab781f84f3106f4f
vkrotova <violettakrotova23@gmail.com>
2025-10-30 15:54:02 -0700

    Created label for checkbox

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ function addElement(name, listname) {
      let newListItem = document.createElement('li');
       let checkbox = document.createElement("input");
 
-    checkbox.type = "checkbox";
-    checkbox.name = "name";
-    checkbox.value = "value";
-    checkbox.id = "id";
+ var label = document.createElement("label");
 
 
 }
