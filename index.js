46cdfd3d0b402074f021dd398c3ab9fb3d863803
vkrotova <violettakrotova23@gmail.com>
2025-10-30 15:58:59 -0700

    Got second  list

--- index.js
+++ index.js
@@ -23,3 +23,5 @@ const ul_brothList = document.getElementById('brothList');
 addElement("1.5 pounds beef short ribs", ul_brothList)
 addElement("¼ large onion, cut into quarters", ul_brothList)
 addElement("1 carrot, cut into big chunks", ul_brothList)
+
+const ul_borschtList = document.getElementById('borschtList');
