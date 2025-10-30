1d99809f7a011e05d03c8e1e2153d85715e99a10
vkrotova <violettakrotova23@gmail.com>
2025-10-30 15:57:22 -0700

    Formated doc

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Function to create checkboxes with labels in a list
 function addElement(name, listname) {
 
-     let newListItem = document.createElement('li');
-      let checkbox = document.createElement("input");
+    let newListItem = document.createElement('li');
+    let checkbox = document.createElement("input");
 
- var label = document.createElement("label");
-  
- label.htmlFor = "id";
- 
- label.appendChild(document.createTextNode(name));
+    var label = document.createElement("label");
 
-  newListItem.appendChild(checkbox);
+    label.htmlFor = "id";
+
+    label.appendChild(document.createTextNode(name));
+
+    newListItem.appendChild(checkbox);
     newListItem.appendChild(label);
 
     listname.appendChild(newListItem);
