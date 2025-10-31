f74b15e229a5bb59e59e65a5aa543f1db07a2e6e
vkrotova <violettakrotova23@gmail.com>
2025-10-30 20:45:46 -0700

    Added oil element

--- index.js
+++ index.js
@@ -21,11 +21,16 @@ function addElement(name, listname) {
 
 const ul_brothList = document.getElementById('brothList');
 
+
+addElement("2 bay leaves", ul_brothList)
+addElement("6-7 black peppercorns", ul_brothList)
+addElement("1 Tbsp salt", ul_brothList)
+addElement("12 cups water", ul_brothList)
 addElement("1.5 pounds beef short ribs", ul_brothList)
 addElement("¼ large onion, cut into quarters", ul_brothList)
 addElement("1 carrot, cut into big chunks", ul_brothList)
 
+
 const ul_borschtList = document.getElementById('borschtList');
 addElement("2 Tbsp oil, divided", ul_borschtList)
-addElement("1 medium onion, diced small", ul_borschtList)
-addElement("2 cloves garlic chopped + 3-4 cloves garlic sliced", ul_borschtList)
+
