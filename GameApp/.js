15adfab2e8510b28aaed354148d520a8105fdd1f
vkrotova <violettakrotova23@gmail.com>
2025-12-02 19:18:22 -0800

    Added padding to center data

--- GameApp/.js
+++ GameApp/.js
@@ -1,6 +1,6 @@
 function resetGame() {
     secretNumber = Math.floor(Math.random() * 10) + 1;
-    attemptCount = 0;
+    attemptCount = 5;
 
     guessHistory.unshift("reset start");
     guessHistory.push("reset end");      
@@ -8,3 +8,7 @@ function resetGame() {
     guessHistory.shift();                
 
     nestedHistory = []; 
+}
+
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetGame);
