64bc19c145e4e8b1b9878ccabfeb407cd69f2f77
vkrotova <violettakrotova23@gmail.com>
2025-12-02 16:01:35 -0800

    Created arrays using the proper syntax

--- GameApp/.js
+++ GameApp/.js
@@ -1,3 +1,10 @@
 function resetGame() {
     secretNumber = Math.floor(Math.random() * 10) + 1;
     attemptCount = 0;
+
+    guessHistory.unshift("reset start");
+    guessHistory.push("reset end");      
+    guessHistory.pop();                   
+    guessHistory.shift();                
+
+    nestedHistory = []; 
