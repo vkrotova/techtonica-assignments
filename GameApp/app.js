3f7aa790372abed80abf368e76fd40c2c85ad8d9
vkrotova <violettakrotova23@gmail.com>
2025-12-05 13:28:06 -0800

    Create arrays using the proper syntax

--- GameApp/app.js
+++ GameApp/app.js
@@ -1,48 +1,46 @@
 let randomNum = Math.floor(Math.random() * 10 + 1);
 let guess = 1;
 let messageSuccess = "Yay, you guessed correctly";
-let messageSmaller = "Not it, try a smaller number";
+let messageSmaller = "Not it, try a smaller number than 0";
 let messageLarger = "Nope, try a larger number";
 
 document.getElementById("submitguess").onclick = function () {
 
-
     let guessNum = document.getElementById("guessField").value;
 
-
     if (guessNum == randomNum) {
-        alert(messageSuccess
-            + guess + " GUESS ");
-    }
-
-
-    else if (guessNum > randomNum) {
+        alert(messageSuccess + guess + " GUESS ");
+    } else if (guessNum > randomNum) {
         guess++;
+        
+        // Array
+        stringSize = messageSmaller.length -1; // Length of the string
+        const charArray = messageSmaller.split(''); // Convert string to array of characters
+        charArray[stringSize] = guessNum; // Modify the character 0 to the guessed number
+        messageSmaller = charArray.join(''); // Join the array back into a string
+
         alert(messageSmaller);
-
-    }
-    else {
+    } else {
         guess++;
-        alert(messageLarger)
 
+        //String
+        alert(messageLarger)
     }
 
 }
 
 
-
-
-document.getElementById("reset-button").addEventListener("click", function() {
-  document.getElementById("reset-button").reset();
+document.getElementById("reset-button").addEventListener("click", function () {
+    document.getElementById("reset-button").reset();
 });
 
 const timeDisplay = document.getElementById("timeDisplay");
 let now = new Date();
-    now.setTime(now.getTime());  
-    timeDisplay.textContent = `Last guess time: ${now.toLocaleTimeString()}`;
+now.setTime(now.getTime());
+timeDisplay.textContent = `Last guess time: ${now.toLocaleTimeString()}`;
 
 
-    function resetGame() {
+function resetGame() {
     let newSecret = Math.floor(Math.random() * 10) + 1;
 
     secretNumber = newSecret;
