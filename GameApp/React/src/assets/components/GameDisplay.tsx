3fe2331ba7c1dc308ea0e58da4f63f1bacc91092
vkrotova <violettakrotova23@gmail.com>
2025-12-12 02:13:50 -0800

    Added Container and top section

---
+++ GameApp/React/src/assets/components/GameDisplay.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const GameDisplay = () => {
+    return(
+        <div className= 'main'>
+            <div className= "container">
+                <div className="topSection"></div> 
+            
+
+            </div>
+
+
+        </div>
+    )
+}
+export default GameDisplay
