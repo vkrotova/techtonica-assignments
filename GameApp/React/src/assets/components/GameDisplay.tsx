ecee1f9a4ac61f4fdcf0a5b437b62fd839c6e6aa
vkrotova <violettakrotova23@gmail.com>
2025-12-12 02:16:28 -0800

    Added top header

--- GameApp/React/src/assets/components/GameDisplay.tsx
+++ GameApp/React/src/assets/components/GameDisplay.tsx
@@ -4,6 +4,10 @@ const GameDisplay = () => {
         <div className= 'main'>
             <div className= "container">
                 <div className="topSection"></div> 
+                <div className="line"></div>
+                <h1>???</h1>
+                <div className="line"></div>
+
             
 
             </div>
