cf8e43e10fa82e91eb091310b239b6a03614f844
vkrotova <violettakrotova23@gmail.com>
2025-12-17 21:51:06 -0800

    Reorganize files and change to function.

--- GameApp/React/src/App.jsx
+++ GameApp/React/src/App.jsx
@@ -1,22 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import GameDisplay from "./components/GameDisplay"
+import GameDisplay from "./components/GameDisplay.jsx"
 
 function App() {
 
-  const [randomNum, setRandonNum] = useState();
-
-
-  let num1 = Math.floor(Math.random() * 10) + 1;
-  const [attempts, setAttempts] = useState(0);
-  const [feedback, setFeedback] = useState("");
-
-
-
   return (
-  
-    <div><GameDisplay /></div>
-    
+
+    <div>
+      <GameDisplay maxNumber={100} />
+    </div>
+
 
   )
 
--- GameApp/React/src/components/GameDisplay.jsx
+++ GameApp/React/src/components/GameDisplay.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
-const GameDisplay = () => (
+import React, { useState } from 'react'
+import './GameDisplay.css'
 
-    <div className='main'>
-        <div className="container">
-            <div className="topSection"></div>
-            <div className="line"></div>
-            <h1>???</h1>
-            <div className="line"></div>
+function GameDisplay({ maxNumber }) {
+    const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random()*max) + 1)
+    const [guess, setGuess] = useState('')
+    const [message, setMessage] = useState(`Guess a number between 1 and ${maxNumber}`)
+
+    
 
 
-            <div className="title">
-                <h1>Guess My Number</h1>
-            </div>
 
-            <div className="middleSection">
-                <p>Score: <span>10</span></p>
-                <p>High Score: <span>10</span></p>
-                <p>Start Guessing</p>
+    return (
+        <div className='main'>
+            <div className="container">
+                <div className="topSection"></div>
+                <div className="line"></div>
+                <h1>???</h1>
+                <div className="line"></div>
 
-                <div className="Inputs">
-                    <input type="number" />
-                    <div className="buttons"></div>
-                    <button>Guess</button>
-                    <button>Reset</button>
+
+                <div className="title">
+                    <h1>Guess My Number</h1>
+                </div>
+
+                <div className="middleSection">
+                    <p>Score: <span>10</span></p>
+                    <p>High Score: <span>10</span></p>
+                    <p>Start Guessing</p>
+
+                    <div className="Inputs">
+                        <input type="number" />
+                        <div className="buttons"></div>
+                        <button>Guess</button>
+                        <button>Reset</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
 export default GameDisplay
