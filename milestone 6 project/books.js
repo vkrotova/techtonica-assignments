11530acf8448cc9888da429b2383a16f6bcacf1b
vkrotova <violettakrotova23@gmail.com>
2026-01-06 13:49:12 -0800

    Hard code data

---
+++ milestone 6 project/books.js
@@ -0,0 +1,17 @@
+const books = [
+  {
+    id:1,
+    title: "Javacript For Kids",
+    author: "Nick Morgan",
+    genre: "Coding",
+    pages: 336
+  },
+  {
+id: 2,
+title: "Eloquent Javascript",
+    author: "Marijn Haverbeke",
+    genre: "Coding",
+    pages: 472
+  }
+
+];
--- milestone 6 project/index.js
+++
@@ -1,2 +0,0 @@
-console.log("Node.js project is ready!");
-
