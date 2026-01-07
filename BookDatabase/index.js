ab85931894aa61baa4f4dffa07210fba197b7d8f
vkrotova <violettakrotova23@gmail.com>
2026-01-07 15:08:35 -0800

    api end point

--- BookDatabase/index.js
+++ BookDatabase/index.js
@@ -3,3 +3,6 @@ import bodyParser from 'body-parser';
 const app = express();
 const PORT = 5001;
 app.use(bodyParser.json());
+
+app.get("/books", (req, res) => { ///api endpoint
+    res.json(BOOKS);
