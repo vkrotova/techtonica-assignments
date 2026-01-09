171c9c8033cf6e90261332acc14076c23753ddec
vkrotova <violettakrotova23@gmail.com>
2026-01-08 20:37:14 -0800

    Requirements commented out

--- BookDatabase/index.js
+++ BookDatabase/index.js
@@ -36,3 +36,13 @@ genre: "Coding",
   }
 
 ];
+
+
+
+
+//Change the information inside the file books.js to have your own unique data (we suggest books, but if you want to use any other data, it's ok)
+//Using Node and Express, create a GET router with a response that converts all using Json()
+//Using that endpoint build a list of all your books in the backend(server side).
+//Using Node and Express, create a route for each one of the verbs in CRUD operations in the backend. You don't need to have a frontend. You can test your API using Postman.
+//Have at least 50 commits
+//Optional Bonus: Using that endpoint build a list of all your books in the frontend (client side). Note: you will need to make a GET request to bring all your information to the frontend too
